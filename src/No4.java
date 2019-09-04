6b7eea88571f541e441cba68df8647d1badd45eb
lateSWE <35782260+lateSWE@users.noreply.github.com>
2019-09-04 21:28:34 +0200

    köhjkbk

--- src/No4.java
+++ src/No4.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class No4 {
     public static void main(String[] args){
 
+    }
+
+    public ArrayList<Object> list;
+    private ArrayList<Object> the_list(Object in){
+        list.add(in);
+        return list;
     }
 }
